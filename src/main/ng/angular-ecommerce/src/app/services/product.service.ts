b70535ef92a733aa30851ca3aa79892378e40dbc
Jesus Villarreal <jesus@jesustvillarreal.com>
2022-11-03 20:54:13 -0500

    updated url in angular product service

--- src/main/ng/angular-ecommerce/src/app/services/product.service.ts
+++ src/main/ng/angular-ecommerce/src/app/services/product.service.ts
@@ -10,9 +10,9 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService { 
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = 'https://redux-springboot-ecommerce.herokuapp.com/api/products';
 
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private categoryUrl = 'https://redux-springboot-ecommerce.herokuapp.com/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
