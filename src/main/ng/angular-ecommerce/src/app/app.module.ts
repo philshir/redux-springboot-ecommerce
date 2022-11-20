3785bae2ba5e08200be568601f755f1a8241b7c5
Jesus Villarreal <jesus@jesustvillarreal.com>
2022-11-19 19:28:28 -0600

    updated angular routes for prod

--- src/main/ng/angular-ecommerce/src/app/app.module.ts
+++ src/main/ng/angular-ecommerce/src/app/app.module.ts
@@ -60,10 +60,8 @@ const routes: Routes = [
 {path: 'category/:id', component: ProductListComponent},
 {path: 'category', component: ProductListComponent},
 {path: 'products', component: ProductListComponent},
-/* {path: '', redirectTo: '/products', pathMatch: 'full'},
-{path: '**', redirectTo: '/products', pathMatch: 'full'}, */
-{path: '', redirectTo: '/', pathMatch: 'full'},
-{path: '**', redirectTo: '/', pathMatch: 'full'},
+{path: '', redirectTo: '/products', pathMatch: 'full'},
+{path: '**', redirectTo: '/products', pathMatch: 'full'},
 ];
 
 @NgModule({
